d04775a110e1f45e5d7bec9cf78dae1ec90ea55d
aman7github <amanmaurya9044@gmail.com>
2025-05-01 14:20:41 +0530

    update resume

--- src/Components/Handleclick.js
+++ src/Components/Handleclick.js
@@ -1,6 +1,12 @@
 
 const Download = () => {
     window.open("https://drive.google.com/file/d/1N2Rg6rycQEV3ru_-E2TwD0DSlPsSJgIq/view?usp=sharing")
-}
+          
 
+
+
+
+
+
+}               
 export default Download
